3118492c6d0ca9177adbbe614fc6788bc43d753f
Vaibhav Kashyap <er.vaibhavkashyap@gmail.com>
2024-09-06 22:02:37 +0530

    Update LSP solution

--- src/main/java/com/solid/principles/exercises/lsp/solution/FixedDepositSolutionAccount.java
+++ src/main/java/com/solid/principles/exercises/lsp/solution/FixedDepositSolutionAccount.java
@@ -4,10 +4,17 @@ package com.solid.principles.exercises.lsp.solution;
  * @author vaibhav.kashyap
  */
 
+// A fixed deposit account does not support withdrawals
 public class FixedDepositSolutionAccount extends BankSolutionAccount {
+    
+    public FixedDepositSolutionAccount(double balance) {
+        super(balance);
+    }
 
-	@Override
-	public void withdraw(double amount) {
-		throw new UnsupportedOperationException("Withdrawals are not allowed on fixed deposit accounts.");
-	}
+    @Override
+    public void deposit(double amount) {
+        throw new UnsupportedOperationException("Fixed deposit accounts cannot accept additional deposits.");
+    }
+
+    // No withdraw method here, as this account type doesn't support withdrawals
 }
